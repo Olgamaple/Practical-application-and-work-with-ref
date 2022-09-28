1205b2a74c4fe1cb3e7e9cf7a7d312ad83fcacbf
Olga Yavorchuk <olyayavorchuk97@gmail.com>
2022-09-28 19:39:04 +0300

    implemented portal functionality

--- src/App.js
+++ src/App.js
@@ -1,45 +1,67 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
 class Form extends Component {
-
-        myRef = React.createRef();
-
-    // componentDidMount() {
-    //    
-    
-    setInputRef = elem => {
-        this.myRef = elem;
+    state = {
+        advOpen: false
+    }
+    componentDidMount() {
+        setTimeout(this.handleClick, 3000)
     }
 
-    focusFirstTI = () => {
-        if (this.myRef) {
-            this.myRef.focus();
-        }
+    handleClick = () => {
+            this.setState(({advOpen}) => ({
+                advOpen: !advOpen
+            }))
     }
 
     render() {
         return (
             <Container>
-                <form className="w-50 border mt-5 p-3 m-auto">
+                <form  onClick={this.handleClick} className="w-50 border mt-5 p-3 m-auto" 
+                style={{'overflow': 'hidden', 
+                        'position': 'relative'}}>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                        <input ref={this.setInputRef} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        <input  type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
                     </div>
                     <div className="mb-3">
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                        <textarea
-                        className="form-control"
-                        id="exampleFormControlTextarea1"
-                        rows="3"
-                        onClick={this.focusFirstTI}>
-                        </textarea>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
+                    {
+                        this.state.advOpen ?
+                            <Portal>
+                                <Msg/>
+                            </Portal> : null 
+                    }
                 </form>
             </Container>
         )
     }
+}
+
+const Portal = (props) => {
+    const node = document.createElement('div');
+    document.body.appendChild(node);
+    
+    return ReactDOM.createPortal(props.children, node);
+}
+
+const Msg = () => {
+    return (
+        <div 
+            style={{'width': '500px', 
+                'height': '150px', 
+                'backgroundColor': 'red', 
+                'position': 'absolute',
+                'left': '50%',
+                'bottom': '20%'}}>
+            Hello
+        </div>
+    )
 }
 
 function App() {
